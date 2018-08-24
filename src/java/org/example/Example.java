13438e66d4e56b0eb311c4f44f191103a9a09751
John Sirois <john.sirois@gmail.com>
2018-08-24 11:01:39 -0600

    Setup and example to test https://github.com/pantsbuild/intellij-pants-plugin/issues/375.

---
+++ src/java/org/example/Example.java
@@ -0,0 +1,6 @@
+package org.example;
+
+class Example {
+  static final int MEANING_OF_LIFE = 42;
+}
+
---
+++ src/java/org/example/ExampleTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+  @Test
+  public void test() {
+    assertEquals(42, Example.MEANING_OF_LIFE);
+  }
+}
